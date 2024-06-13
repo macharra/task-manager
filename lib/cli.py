fd96b71f4a19ff36cd2f4f12d4fe936dcd3d6502
rogers.machara <rogersmachara13@gmail.com>
2024-06-13 09:18:23 +0300

    setting up cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,27 +1,53 @@
 # lib/cli.py
 
+from sqlalchemy.orm import sessionmaker
+from models import Employee, Task, Project, engine
 from helpers import (
     exit_program,
     helper_1
-)
+) 
 
+Session = sessionmaker(bind=engine)
 
 def main():
+    session = Session()
+
     while True:
         menu()
         choice = input("> ")
+
         if choice == "0":
-            exit_program()
-        elif choice == "1":
             helper_1()
+        elif choice == "1":
+            manage_employees(session)
+        elif choice == "2":
+            manage_projects(session)
+        elif choice == "3":
+            manage_tasks(session)
+        elif choice == "4":
+            exit_program()
         else:
             print("Invalid choice")
 
 
 def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+    print("\t\t EMPLOYEE TASK MANAGER \t\t")
+    print("Please select an option(0-4):")
+    print("0. Help")
+    print("1. Manage Employees")
+    print("2. Manage Projects")
+    print("3. Manage Tasks")
+    print("4. Exit the program")
+
+def manage_employees():
+    pass
+
+def manage_projects():
+    pass
+
+def manage_tasks():
+    pass
+
 
 
 if __name__ == "__main__":
